0c0b03dcf5104b87e62b504f7f2ec6839d64a65d
matrixnad <matrix.nad@gmail.com>
2017-11-07 19:43:41 +0000

    first on fork

--- example.php
+++ example.php
@@ -5,16 +5,16 @@
  * Date: 1/24/17
  * Time: 4:53 PM
  */
-include('./docx_reader.php');
+require_once("vendor/autoload.php");
 
-$doc = new Docx_reader();
+$doc = new \Docx_reader\Docx_reader();
 $doc->setFile('./sample.docx');
 
 if(!$doc->get_errors()) {
     $html = $doc->to_html();
     $plain_text = $doc->to_plain_text();
 
-    echo $html;
+    file_put_contents("/var/www/html/testing.html", $html);
 } else {
     echo implode(', ',$doc->get_errors());
 }
